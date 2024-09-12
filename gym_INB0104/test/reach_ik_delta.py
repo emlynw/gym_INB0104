f028b901578eb2e29936fbc439cfa962a40e7bfe
emlynw <emlynw98@gmail.com>
2024-09-12 15:07:33 +0100

    reward function checks for collision between finger pads and objects

--- gym_INB0104/test/reach_ik_delta.py
+++ gym_INB0104/test/reach_ik_delta.py
@@ -6,9 +6,9 @@ import numpy as np
 
 def main():
     render_mode = "rgb_array"
-    env = gym.make("gym_INB0104/reach_ik_delta", render_mode=render_mode)
+    env = gym.make("gym_INB0104/reach_ik_delta", render_mode=render_mode, randomize_domain=False)
     env = TimeLimit(env, max_episode_steps=200)    
-    camera_id = 1
+    camera_id = 0
     waitkey = 10
 
     while True:
@@ -17,20 +17,31 @@ def main():
         terminated = False
         truncated = False
         obs, info = env.reset()
+        rotate = False
         if render_mode == "rgb_array":
             pixels = obs["images"]["front"]
             cv2.resize(pixels, (224, 224))
             cv2.imshow("pixels", cv2.resize(cv2.cvtColor(pixels, cv2.COLOR_RGB2BGR), (720, 720)))
             cv2.waitKey(waitkey)
         while not terminated and not truncated:
-            if i < 50:
-                action = np.array([0.5, -0.5, 0.0, -1.0])
-            elif i < 100:
-                action = np.array([0.0, 0.0, -0.5, 1.0])
-            elif i < 150:
-                action = np.array([-0.5, 0.0, 0.0, -1.0])
-            elif i < 200:
-                action = np.array([0.0, 0.5, 0.0, 1.0])
+            if rotate:
+                if i < 50:
+                    action = np.array([0.0, 0.0, 0.0, 1.0, 1.0, 1.0, -1.0])
+                elif i < 100:
+                    action = np.array([0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0])
+                elif i < 150:
+                    action = np.array([0.0, 0.0, 0.0, -1.0, -1.0, -1.0, -1.0])
+                elif i < 200:
+                    action = np.array([0.0, 0.0, 0.0, -1.0, -1.0, -1.0, 1.0])
+            else:
+                if i < 15:
+                    action = np.array([0.2, 0.0, 0.0, 0.0, 0.0, 0.0, -1.0])
+                elif i < 50:
+                    action = np.array([0.0, 0.0, -1.0, 0.0, 0.0, 0.0, -1.0])
+                elif i < 80:
+                    action = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0])
+                elif i < 100:
+                    action = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -1.0])
             
             obs, reward, terminated, truncated, info = env.step(action)
             if render_mode == "rgb_array":
@@ -40,7 +51,5 @@ def main():
                 cv2.waitKey(waitkey)
             i+=1
         
-
-
 if __name__ == "__main__":
     main()
