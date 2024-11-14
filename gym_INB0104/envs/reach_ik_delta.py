b5fbf59e431b680e058b66933079db6ec9371658
emlynw <emlynw98@gmail.com>
2024-11-14 13:29:39 +0000

    fixed resolution

--- gym_INB0104/envs/reach_ik_delta.py
+++ gym_INB0104/envs/reach_ik_delta.py
@@ -30,7 +30,7 @@ class ReachIKDeltaEnv(MujocoEnv, utils.EzPickle):
         ee_dof = 6, # 3 for position, 3 for orientation
         control_dt=0.1,
         physics_dt=0.002,
-        width=640,
+        width=480,
         height=480,
         pos_scale=0.1,
         rot_scale=0.05,
