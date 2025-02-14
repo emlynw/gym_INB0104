8f84d2e2545a38c71adcb3eefa126660747a979d
emlynw <emlynw98@gmail.com>
2025-02-14 17:22:20 +0000

    sim to real calibration

--- gym_INB0104/test/calibration_sim.py
+++ gym_INB0104/test/calibration_sim.py
@@ -8,19 +8,24 @@ np.set_printoptions(suppress=True)
 
 def main():
     render_mode = "rgb_array"
-    env = gym.make("gym_INB0104/ReachIKDeltaEnv", render_mode=render_mode, randomize_domain=False, ee_dof=4, pos_scale= 0.1, rot_scale = 0.05, )
-    env = TimeLimit(env, max_episode_steps=200)    
+    env = gym.make("gym_INB0104/ReachStrawbEnv", render_mode=render_mode, randomize_domain=False, ee_dof=6, pos_scale= 0.01, rot_scale = 0.2)
+    env = TimeLimit(env, max_episode_steps=50)    
     waitkey = 1
 
     obs, info = env.reset()
-    for i in range(5):
-        action = np.array([1.0, 0.0, 0.0, 0.0, 0.0])
+    initial_pose = obs['state']['tcp_pose']
+    print(f"initial pose: {initial_pose}")
+    for i in range(40):
+        action = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0])
         obs, reward, terminated, truncated, info = env.step(action)
-        print(obs['state']['panda/tcp_pos'])
+        print(obs['state']['tcp_pose'])
         if render_mode == "rgb_array":
                 pixels = obs["images"]["front"]
                 cv2.imshow("pixels", cv2.resize(cv2.cvtColor(pixels, cv2.COLOR_RGB2BGR), (720, 720)))
                 cv2.waitKey(waitkey)
+    final_pose = obs['state']['tcp_pose']
+    print(f"final pose: {final_pose}")
+    print(f"diff: {final_pose-initial_pose}")
         
         
 if __name__ == "__main__":
--- gym_INB0104/test/gamepad_wrapper_test.py
+++ gym_INB0104/test/gamepad_wrapper_test.py
@@ -6,13 +6,14 @@ import numpy as np
 from gamepad_wrapper import GamepadIntervention
 from serl_launcher.wrappers.serl_obs_wrappers import SERLObsWrapper
 import time
+np.set_printoptions(suppress=True)
 
 def main():
     render_mode = "rgb_array"
     env = gym.make("gym_INB0104/ReachStrawbEnv", render_mode=render_mode, randomize_domain=True, reward_type="sparse", ee_dof=6)
-    env = SERLObsWrapper(env)
+    # env = SERLObsWrapper(env)
     env = GamepadIntervention(env)
-    env = TimeLimit(env, max_episode_steps=200)    
+    env = TimeLimit(env, max_episode_steps=500)    
     waitkey = 10
     cameras = ['wrist1', 'wrist2', 'front']
     resize_resolution = (480, 480)
@@ -24,11 +25,21 @@ def main():
         truncated = False
         obs, info = env.reset()
         rotate = True
+        print("press any key to continue")
+        for camera in cameras:
+            if camera=="wrist1":
+                cv2.imshow(camera, cv2.resize(cv2.cvtColor(cv2.rotate(obs['images'][camera], cv2.ROTATE_180), cv2.COLOR_RGB2BGR), resize_resolution))
+            else:
+                cv2.imshow(camera, cv2.resize(cv2.cvtColor(obs['images'][camera], cv2.COLOR_RGB2BGR), resize_resolution))
+        cv2.waitKey(0)
         
         while not terminated and not truncated:
             step_start_time = time.time()
             for camera in cameras:
-                cv2.imshow(camera, cv2.resize(cv2.cvtColor(obs[camera], cv2.COLOR_RGB2BGR), resize_resolution))
+                if camera=="wrist1":
+                    cv2.imshow(camera, cv2.resize(cv2.cvtColor(cv2.rotate(obs['images'][camera], cv2.ROTATE_180), cv2.COLOR_RGB2BGR), resize_resolution))
+                else:
+                    cv2.imshow(camera, cv2.resize(cv2.cvtColor(obs['images'][camera], cv2.COLOR_RGB2BGR), resize_resolution))
                 cv2.waitKey(waitkey)
     
             action = np.zeros_like(env.action_space.sample())
@@ -37,10 +48,10 @@ def main():
 
 
             obs, reward, terminated, truncated, info = env.step(action)
-            print(reward)
+            print(obs['state']['tcp_pose'])
             step_time = time.time()-step_start_time
-            if step_time < 0.1:
-                time.sleep(0.1 - step_time)
+            if step_time < 0.05:
+                time.sleep(0.05 - step_time)
             i+=1
 
 if __name__ == "__main__":
